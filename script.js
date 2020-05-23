456cfbf58187eea9e57630da4a344c2b3c2b6664
Louis Maxwell <44560811+Maxwellvincent@users.noreply.github.com>
2020-05-23 01:30:32 -0400

    created js.file with stored questions

---
+++ script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "A solution with a pH of 4 would be considered?",
+        choices:["Acidic","Basic","Neutral","Ionic"],
+        answer: 0
+    },
+    {
+        question: "A covalent bond is what type of sharing?",
+        choices:["Equal sharing between to atoms","Unequal sharing between atoms", "No sharing between atoms", "Ionic bonding"],
+        answer: 0
+    },
+    {
+        question: "Hydrogen bonding is experienced between what molecules?",
+        choices: ["Metals","Salts","H,N,O,F","Group 1 elements"],
+        answer: 2
+    },
+    {
+        question: "What is STP?",
+        choices: ["Universal gas law", "The scientific method", "A hypothesis","Standard Temperature and pressure"],
+        answer: 3
+    },
+    {
+        question: "What is the powerhouse of the cell?",
+        choices: ["Cell wall", "Mitochondria","ER","Nucleus"],
+        answer: 1
+    }
+]
