8b235aef022dbaf2ba0640c7be6e42405171e27d
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-06-03 02:35:35 -0400

    This is a re-work of the orignal app,have created a new branch to implement new work for functionality of app, more user friendly with keyboard, added submit button to quiz app

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const startButton = $("#start-button");
 const nextButton = $("#next-button");
+const submitButton = $("#submit-button");
 const questionElement = $("#question");
 let score = 0;
 // undeclared variable random question
@@ -39,6 +40,7 @@ const questions = [
 // this function removes the hide class from all the elements. 
 function removeHide(){
     $(".question-container").removeClass("hide");
+    submitButton.removeClass("hide");
     $("#question").removeClass("hide");
     $("fieldset").removeClass("hide");
     $(".input-cnt").removeClass("hide");
@@ -63,6 +65,7 @@ function startQuiz(){
 
         updateResults();
     });
+
 }
 
 
@@ -73,6 +76,7 @@ function nextQuestion(){
 
     showQuestion(randomQuestion[currentQuestionIndex]);
     nextButton.addClass("hide");
+    submitButton.removeClass("hide");
 }
 
 
@@ -90,44 +94,96 @@ function showQuestion(questions){
             </fieldset>
         `);
     });
-
+    let radioBtn = $(":radio").toArray();
     let answerForm = $('fieldset');
     let arrayOfAnswers = Array.from(answerForm);
     console.log(arrayOfAnswers);
         // this functions adds a click event to each answer-input
         // may need to add this to each new question, bc it runs once?
-        answerForm.on('click', 'input', function(e){
-            nextButton.removeClass("hide");
-            // If the radio button that is clicked is checked(true), then
-            // we want to see if the value (this.value) is equal to the question.answer
-            // If it is then, run this function questionCorrect
-            // also increase score by 1
-            // then show next button, to generate next question
-            if(this.checked === true){
-                answerChoices();
-            }
+        submitButton.on("click", () => {
 
-            if(this.value == correctAnswer){
+            if(checkButton() == false){
+                alert("please select button!");
+            }
+            
+            let chosenAnswer; 
+            radioBtn.filter(function(button) {
+                if(button.checked == true) {
+                    console.log(button.value);
+                    chosenAnswer = button.value;
+                }
+            });
+            
+            if(chosenAnswer == correctAnswer){
                 questionCorrect();
                 // need to increase the score and the currentindex
                 score++
-            } else if(this.value != correctAnswer){
-                // find the fieldset with the correct answer
-                arrayOfAnswers.forEach((item) => {
-                    if(item.id == correctAnswer){
-                        console.log(item.setAttribute("class", "right"));
-                    }
-                })
-                questionWrong();
+                submitButton.addClass("hide");
+                nextButton.removeClass("hide");
+            } else {
+                // Need to implement code for wrong answers, also need to add classes
+                // when finish style the App larger text!!
+                
             }
 
-
+            //  else {
+            //     radioBtn.forEach((button) => {
+            //         if(button.checked != true){
+            //             button.disabled = true;
+            //         }
+            //     });
+            //     console.log("THIS CLICKED");
+            // }
+ 
+        
         });
+        // answerForm.on('click', 'input', function(e){
+        //     nextButton.removeClass("hide");
+        //     // If the radio button that is clicked is checked(true), then
+        //     // we want to see if the value (this.value) is equal to the question.answer
+        //     // If it is then, run this function questionCorrect
+        //     // also increase score by 1
+        //     // then show next button, to generate next question
+        //     if(this.checked === true){
+        //         answerChoices();
+        //     }
+
+        //     if(this.value == correctAnswer){
+        //         questionCorrect();
+        //         // need to increase the score and the currentindex
+        //         score++
+        //     } else if(this.value != correctAnswer){
+        //         // find the fieldset with the correct answer
+        //         arrayOfAnswers.forEach((item) => {
+        //             if(item.id == correctAnswer){
+        //                 console.log(item.setAttribute("class", "right"));
+        //             }
+        //         })
+        //         questionWrong();
+        //     }
+
+
+        // });
 
 }
 
-// Next grab the next button add a click event
+// makes sure a button is clicked
+function checkButton(){
+    let radioBtn = $(":radio").toArray();
+    let sum = 0;
+    radioBtn.forEach((button) => {
+        if(button.checked === false){
+            sum++;
+        }
+    });
+    if(sum < 4){
+        return true;
+    } else {
+        return false;
+    }
+}
 
+// Next grab the next button add a click event
 nextButton.on("click", (e)=>{
     currentQuestionIndex++;
 
